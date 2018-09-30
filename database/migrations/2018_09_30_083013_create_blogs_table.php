505b94063ffd2648eadce0e450cfbab759d0adcd
Manish Mahant <manishmahant>
2018-09-30 14:54:11 +0530

    initial setup

---
+++ database/migrations/2018_09_30_083013_create_blogs_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBlogsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('blogs', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('user_id')->default(null)->nullable();
+            $table->string('title',190)->default(null)->nullable();
+            $table->text('details')->default(null)->nullable();
+            $table->dateTime('created_at')->default(null)->nullable();
+            $table->dateTime('updated_at')->default(null)->nullable();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade')->onUpdate('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('blogs');
+    }
+}
