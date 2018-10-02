bcdb075b6fae2aae2e9d5fabfac1d3b72e6fa1cc
Manish Mahant <manishmahant>
2018-10-02 11:10:55 +0530

    react manifest

--- resources/js/app.js
+++ resources/js/app.js
@@ -15,7 +15,7 @@ require('./bootstrap');
 
 
 // require('./index');
-
+import React from "react"
 import { render } from "react-dom"
 import { Provider } from 'react-redux'
 import store from './store'
