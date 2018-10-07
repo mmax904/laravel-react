c874fff9cf645f699e00f1f62674c17cfb2ef5a2
Manish Mahant <manishmahant>
2018-10-07 16:03:31 +0530

    changes in store

--- resources/js/store/config.js
+++ resources/js/store/config.js
@@ -7,42 +7,61 @@ import thunk from 'redux-thunk'
 import rootReducer from './reducers'
 import logger from '../middlewares/logger'
 import crashReporter from '../middlewares/crashReporter'
+import RedirectIfAuthenticated from '../middlewares/RedirectIfAuthenticated'
 import monitorReducerEnhancer from '../enhancers/monitorReducer'
 // import * as allMiddleWares from '../middlewares/onePlaceForAll'
 import { timeoutScheduler } from '../middlewares/onePlaceForAll'
 
+//var browserHistory = require("history").createBrowserHistory
+//import createHistory from "history/createBrowserHistory"
+//import { createBrowserHistory as browserHistory } from 'history';
+//import browserHistory from '../history/history'
+import { createBrowserHistory } from "history";
+
+import {
+	ConnectedRouter,
+	syncHistoryWithStore,
+	routerMiddleware
+} from 'react-router-redux'
+
+// Build the middleware for intercepting and dispatching navigation actions
+// const routeMiddleware = routerMiddleware(browserHistory);
+const routeMiddleware = routerMiddleware(createBrowserHistory());
+
 //const loggerMiddleware = createLogger();
 const middlewares = [
-    thunk,
-    logger,
-    crashReporter,
-    timeoutScheduler
+	thunk,
+	routeMiddleware,
+	logger,
+	crashReporter,
+	timeoutScheduler,
+	RedirectIfAuthenticated
 ];
 //middlewares.push(loggerMiddleware);
 //const storeConfig = (initialState = {}) => {
-export default function (initialState = {}) {
-  // Middleware and store enhancers
-  const enhancers = [
-    applyMiddleware(...middlewares),
-  ]
-  
-  if (process.env.NODE_ENV !== 'production') {
-    //enhancers.push(applyMiddleware(loggerMiddleware))
-    enhancers.push(monitorReducerEnhancer)
-    window.devToolsExtension && enhancers.push(window.devToolsExtension())
-  }
-  
-  const store = createStore(rootReducer, initialState, compose(...enhancers))
-  
-  // For hot reloading reducers
-  if (module.hot) {
-    // Enable Webpack hot module replacement for reducers
-    module.hot.accept('./reducers', () => {
-      const nextReducer = require('./reducers').default // eslint-disable-line global-require
-      store.replaceReducer(nextReducer)
-    })
-  }
-  return store 
+export default function (history, initialState = {}) {
+	// Middleware and store enhancers
+	const enhancers = [
+		applyMiddleware(...middlewares)
+	]
+
+	if (process.env.NODE_ENV !== 'production') {
+		//enhancers.push(applyMiddleware(loggerMiddleware))
+		enhancers.push(monitorReducerEnhancer)
+		window.devToolsExtension && enhancers.push(window.devToolsExtension())
+	}
+
+	const store = createStore(rootReducer, initialState, compose(...enhancers))
+
+	// For hot reloading reducers
+	if (module.hot) {
+		// Enable Webpack hot module replacement for reducers
+		module.hot.accept('./reducers', () => {
+			const nextReducer = require('./reducers').default // eslint-disable-line global-require
+			store.replaceReducer(nextReducer)
+		})
+	}
+	return store
 }
 
 //export default storeConfig
--- resources/js/store/reducers.js
+++ resources/js/store/reducers.js
@@ -1,7 +1,15 @@
 import { combineReducers } from 'redux'
+import {
+    routerReducer
+} from 'react-router-redux'
 
 import auth from '../modules/auth/store/reduer'
 import user from '../modules/user/store/reducer'
 import articles from '../modules/article/store/reducer'
 
-export default combineReducers({ auth, user, articles })
+export default combineReducers({
+	auth,
+	user,
+	articles,
+	router: routerReducer 
+})
